804ce916bc8128532811e38fcde60dc77c6c48d0
MarunNexit <mazkevych2003@gmail.com>
2024-08-11 20:54:10 +0300

    url and text QR Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {QrCodeComponent} from "./components/qr-code/qr-code.component";
+import {HeaderComponent} from "./components/header/header.component";
+import {FooterComponent} from "./components/footer/footer.component";
+import {ThemeService} from "./services/theme.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, QrCodeComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [ThemeService]
 })
 export class AppComponent {
   title = 'QRCodeGenerator';
---
+++ src/app/models/qr-code-types.enum.ts
@@ -0,0 +1,7 @@
+export enum QrCodeType {
+  URL = 'Link',
+  TEXT = 'Text',
+  EMAIL = 'E-mail',
+  IMAGE = 'Image',
+  WIFI = 'Wi-Fi',
+}
